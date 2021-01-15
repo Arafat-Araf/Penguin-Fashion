19251c9620627ee0bc423da4b7389f3c6fcbfed2
Arafat-Araf <76771684+Arafat-Araf@users.noreply.github.com>
2021-01-15 23:44:06 +0600

    add

---
+++ index.php
@@ -0,0 +1,321 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Title Tag -->
+    <title>Penguin -Fashion website. </title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+
+    <!-- Google Fonts -->
+   
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;500&family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+
+    <!-- Main Stylesheet -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+    <!-- Header Navbar -->
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light-pink py-3">
+        <div class="container">
+            <a class="navbar-brand" href="#"><img src="images/img/Logo.png" alt=""></a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Products</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About Us</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact Us</a>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+    <!-- End Header Navbar -->
+
+    <!-- Banner Section -->
+    <section class="banner py-100 bg-light-pink">
+        <div class="container">
+            <div class="row align-items-center justify-content-between">
+                <!-- Banner Text -->
+                <div class="col-md-5">
+                    <div class="banner-text mb-5 mb-md-0">
+                        <h1 class="text-uppercase"><span class="text-dark d-block">BackPack</span> Moon</h1>
+                        <p class="py-3">Lorem Ipsum is simply dummy text of the 
+                            printing and typesetting industry.</p>
+                        <a href="" class="btn btn-primary btn-lg"><i class="fa fa-shopping-cart me-2 "></i> BUY NOW</a>
+                    </div>
+                </div>
+                <!-- Banner Image -->
+                <div class="col-md-5">
+                    <div class="banner-img">
+                        <img class="img-fluid" src="images/img/banner.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Banner Section -->
+    
+    <!-- Help Section -->
+    <section class="help-section py-100">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-5 order-2">
+                    <!-- Help Card -->
+                    <div class="card p-3 rounded shadow border-0 mb-4">
+                        <div class="row g-0 align-items-center text-center text-md-start">
+                            <div class="col-md-2">
+                                <img class="width-100 img-fluid" src="images/img/icons/icon1.png" alt="...">
+                            </div>
+                            <div class="col-md-10">
+                                <div class="card-body">
+                                    <h5 class="card-title">Find the Perfect Fit</h5>
+                                    <p class="card-text">Everybody is different, which is why we offer styles for every body.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- End Help Card -->
+
+                    <!-- Help Card -->
+                    <div class="card p-3 rounded shadow border-0 mb-4">
+                        <div class="row g-0 align-items-center text-center text-md-start">
+                            <div class="col-md-2">
+                                <img class="width-100 img-fluid" src="images/img/icons/icon2.png" alt="...">
+                            </div>
+                            <div class="col-md-10">
+                                <div class="card-body">
+                                    <h5 class="card-title">Free Exchanges</h5>
+                                    <p class="card-text">One of the many reasons you can shop with peace of mind.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                     <!-- End Help Card -->
+
+                    <!-- Help Card -->
+                    <div class="card p-3 rounded shadow border-0 mb-4">
+                        <div class="row g-0 align-items-center text-center text-md-start">
+                            <div class="col-md-2 col-12">
+                                <img class="width-100 img-fluid" src="images/img/icons/icon3.png" alt="...">
+                            </div>
+                            <div class="col-md-10 col-12">
+                                <div class="card-body">
+                                    <h5 class="card-title">Contact Our Seller</h5>
+                                    <p class="card-text">They are here to help you. That's quite literally what we pay them for.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- End Help Card -->
+
+                </div>
+                <!-- Help Image -->
+                <div class="col-lg-7 order-lg-2">
+                    <img class="p-5 img-fluid" src="images/img/help.png" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End Help Section -->
+
+
+    <!-- casual backpack Section -->
+    <section class="py-100">
+        <div class="container">
+            <h2 class="h1 text-uppercase text-center text-md-start mb-5">Casual backpack</h2>
+            <div class="row">
+                <!-- Product Card -->
+                <div class="col-lg-4">
+                    <div class="product card p-3 mb-3 mb-lg-0 border-0 rounded shadow">
+                        <img src="images/img/products/schoolbag.png" class="rounded card-img-top" alt="...">
+                        <div class="px-0 pb-0 card-body">
+                          <h5 class="card-title">School backpack </h5>
+                          <p class="card-text">Lorem Ipsum is simply dummy text of the 
+                            printing and typesetting industry.</p>
+                          <div class="d-flex justify-content-between align-items-center">
+                              <div class="price">
+                                  <span>$234</span>
+                              </div>
+                              <div class="cart">
+                                  <a href="#" class="btn btn-primary btn-lg"> <i class="fa fa-shopping-cart me-2 "></i> BUY NOW</a>
+
+                              </div>
+                          </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- End Product Card -->
+
+                <!-- Product Card -->
+                <div class="col-lg-4">
+                    <div class="product card p-3 mb-3 mb-lg-0 border-0 rounded shadow">
+                        <img src="images/img/products/schoolbag1.png" class="rounded card-img-top" alt="...">
+                        <div class="px-0 pb-0 card-body">
+                          <h5 class="card-title">School backpack </h5>
+                          <p class="card-text">Lorem Ipsum is simply dummy text of the 
+                            printing and typesetting industry.</p>
+                          <div class="d-flex justify-content-between align-items-center">
+                              <div class="price">
+                                  <span>$134</span>
+                              </div>
+                              <div class="cart">
+                                  <a href="#" class="btn btn-primary btn-lg"> <i class="fa fa-shopping-cart me-2 "></i> BUY NOW</a>
+
+                              </div>
+                          </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- End Product Card -->
+
+                <!-- Product Card -->
+                <div class="col-lg-4">
+                    <div class="product card p-3 mb-3 mb-lg-0 border-0 rounded shadow">
+                        <img src="images/img/products/securitybag.png" class="rounded card-img-top" alt="...">
+                        <div class="px-0 pb-0 card-body">
+                          <h5 class="card-title">CityLite Security Backpack </h5>
+                          <p class="card-text">Lorem Ipsum is simply dummy text of the 
+                            printing and typesetting industry.</p>
+                          <div class="d-flex justify-content-between align-items-center">
+                              <div class="price">
+                                  <span>$155</span>
+                              </div>
+                              <div class="cart">
+                                  <a href="#" class="btn btn-primary btn-lg"> <i class="fa fa-shopping-cart me-2 "></i> BUY NOW</a>
+
+                              </div>
+                          </div>
+                        </div>
+                    </div>
+                    <!-- End Product Card -->
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <!-- casual backpack Section end-->
+
+    <!-- Travel backpack Section -->
+    <section>
+        <div class="container">
+            <h2 class="h1 text-uppercase text-center text-md-start mb-5">Travel backpack</h2>
+            <div class="row">
+
+                <!-- Product Card -->
+                <div class="col-lg-4">
+                    <div class="card p-3 mb-3 mb-lg-0 border-0 rounded shadow">
+                        <img src="images/img/products/backpacking-travel.png" class="rounded card-img-top" alt="...">
+                        <div class="px-0 pb-0 card-body">
+                          <h5 class="card-title">Travel Sport</h5>
+                          <p class="card-text">Lorem Ipsum is simply dummy text of the 
+                            printing and typesetting industry.</p>
+                          <div class="d-flex justify-content-between align-items-center">
+                              <div class="price">
+                                  <span>$248</span>
+                              </div>
+                              <div class="cart">
+                                  <a href="#" class="btn btn-primary btn-lg"> <i class="fa fa-shopping-cart me-2 "></i> BUY NOW</a>
+
+                              </div>
+                          </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- End Product Card -->
+
+                <!-- Product Card -->
+                <div class="col-lg-4">
+                    <div class="card p-3 mb-3 mb-lg-0 border-0 rounded shadow">
+                        <img src="images/img/products/backpacking-travel1.png" class="rounded card-img-top" alt="...">
+                        <div class="px-0 pb-0 card-body">
+                          <h5 class="card-title">Jack Wolfskin West Highland </h5>
+                          <p class="card-text">Lorem Ipsum is simply dummy text of the 
+                            printing and typesetting industry.</p>
+                          <div class="d-flex justify-content-between align-items-center">
+                              <div class="price">
+                                  <span>$198</span>
+                              </div>
+                              <div class="cart">
+                                  <a href="#" class="btn btn-primary btn-lg"> <i class="fa fa-shopping-cart me-2 "></i> BUY NOW</a>
+
+                              </div>
+                          </div>
+                        </div>
+                    </div>
+                </div> 
+                <!-- End Product Card -->
+
+                <!-- Product Card -->
+                <div class="col-lg-4">
+                    <div class="card p-3 mb-3 mb-lg-0 border-0 rounded shadow">
+                        <img src="images/img/products/backpacking-travel2.png" class="rounded card-img-top" alt="...">
+                        <div class="px-0 pb-0 card-body">
+                          <h5 class="card-title">Hiking Trekking Bag Camping</h5>
+                          <p class="card-text">Lorem Ipsum is simply dummy text of the 
+                            printing and typesetting industry.</p>
+                          <div class="d-flex justify-content-between align-items-center">
+                              <div class="price">
+                                  <span>$152</span>
+                              </div>
+                              <div class="cart">
+                                  <a href="#" class="btn btn-lg btn-primary"> <i class="fa fa-shopping-cart me-2 "></i> BUY NOW</a>
+
+                              </div>
+                          </div>
+                        </div>
+                    </div>
+                </div>
+                <!--  Product Card End-->
+            </div>
+        </div>
+    </section>
+    <!-- Travel backpack Section end -->
+
+
+    <!-- Subscribe Section -->
+    <section class="subscribe py-100">
+        <div class="container">
+            <h2 class="h1 text-uppercase text-center mb-5">Subscribe</h2>
+            <div class="row">
+                <div class="col-lg-5 mx-auto text-center">
+                    <form class="input-group shadow-subscribe rounded">
+                        <input class="form-control" type="email" placeholder="Enter your email">
+                        <span class="">
+                            <button class="btn btn-primary" type="submit">Subscribe</button>
+                        </span>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Subscribe Section end -->
+    <footer>Arafat@All right reserve</footer>
+    <!-- Bootstrap JS -->
+    <script src="assets/js/bootstrap.min.js"></script>
+</body>
+</html>
